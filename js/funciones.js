20bce6bd9ca128afb27a10b6becfc7e99db9c73a
Connor <cony.castillo03@gmail.com>
2021-04-15 17:12:08 -0400

    agregar tabla js y prueba propia

---
+++ js/funciones.js
@@ -0,0 +1,6 @@
+document.getElementById("btn_registrar").onclick= Registrar;
+function Registrar(){
+    var nombre = document.getElementById("registra").value;
+    var descripcion = document.getElementById("desc").valua;
+    alert("Nombre: "+ nombre + "\ndescripcion: "+ descripcion);
+}
